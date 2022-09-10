91bfcd1b2fc352fe607f7e74338ccd1a489e0be3
Katie <thompsonkatherine704@gmail.com>
2022-09-10 12:22:33 -0400

    added comments for clarity to js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-// Use Callback function, which is a function following a method
+// Use Callback,  a function following a method, to loop over all time entries
 $(document).ready(function () {
+  // get custom date format using moment
   var time = moment().format("dddd, MMMM Do, YYYY");
+  // display custom date to page
   $("#currentDay").text(time);
   console.log(time);
+  // create loop over each business hour, starting at 9am (i=9) ending at 6pm
   for (var i = 9; i < 19; i++) {
+    // link to CSS for past, present, and future color-coding
     var colorKey = "";
     var currentHour = moment().hours();
     if (currentHour > i) {
@@ -13,6 +17,7 @@ $(document).ready(function () {
     } else {
       colorKey = "future";
     }
+    // convert displayed hour to AM /PM
     var hourDisplay = "";
     if (i < 12) {
       hourDisplay = i + "AM";
@@ -21,6 +26,8 @@ $(document).ready(function () {
     } else {
       hourDisplay = i - 12 + "PM";
     }
+    // section out display into 3 total columns for: displayed hour(2/12), text area(8/12), and save button (2/12)
+    // add class attributes for sectioning via bootstrap
     var row = $("<div>").addClass("row time-block").attr("id", i);
     var hourEl = $("<div>").addClass("col-2 hour").text(hourDisplay);
     var textArea = $("<textarea>")
@@ -35,6 +42,7 @@ $(document).ready(function () {
         var activity = $(this).parent().siblings(".description").val();
         localStorage.setItem(hourKey, activity);
       });
+    // save button icon using font awesome
     var icon = $("<i>").addClass("fas fa-save");
     $(".container").append(
       row.append(hourEl, textArea, buttonDiv.append(button.append(icon)))
